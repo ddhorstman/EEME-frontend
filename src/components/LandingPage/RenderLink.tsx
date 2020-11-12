9a2e348cbc30f1c5a40ad68701985ef99a22b012
David Horstman <davidzilla27@gmail.com>
2020-11-12 15:27:50 -0800

    Fix types to work with ButtonClick. Prepare for copy to clipboard functionality

--- src/components/LandingPage/RenderLink.tsx
+++ src/components/LandingPage/RenderLink.tsx
@@ -1,20 +1,44 @@
-import { TableRow, TableCell, Link } from "@material-ui/core";
+import { TableRow, TableCell, Link, Button } from "@material-ui/core";
+import { FileCopy } from "@material-ui/icons";
 import React from "react";
 import { EncodedLink } from "../../types/dataExchangeTypes";
+import { ButtonClickHandler } from "../../types/formTypes";
 
 interface Props {
   link: EncodedLink;
 }
 
 const RenderLink: React.FC<Props> = ({ link }) => {
+  const eemeHref = `https://eeme.me/${link.encodedPath}`;
+  const textAreaRef = React.useRef<HTMLTextAreaElement>(null);
+
+  const handleCopy: ButtonClickHandler = ({ target }) => {
+    textAreaRef?.current?.select();
+    console.log(textAreaRef?.current?.value);
+    document.execCommand("copy");
+    target.focus();
+  };
+
   return (
     <TableRow key={link.id}>
       <TableCell align='center'>
-        <Link href={`https://eeme.me/${link.encodedPath}`} target='_blank'>
+        <Link href={eemeHref} target='_blank'>
+          <textarea
+            ref={textAreaRef}
+            style={{ display: "none" }}
+            value={eemeHref}
+          />
           eeme.me/{link.encodedPath}
         </Link>
+        <Button onClick={handleCopy}>
+          <FileCopy />
+        </Button>
       </TableCell>
-      <TableCell align='center'>{link.target}</TableCell>
+      <TableCell align='center'>
+        <Link href={link.target} target='_blank'>
+          {link.target}
+        </Link>
+      </TableCell>
     </TableRow>
   );
 };
--- src/types/formTypes.ts
+++ src/types/formTypes.ts
@@ -3,21 +3,29 @@ import { FormEvent, SyntheticEvent } from "react";
 /**
  * Extended EventTarget which includes 'name' and 'value' fields.
  */
-interface InputEventTarget extends EventTarget {
+interface CustomEventTarget extends EventTarget {
   name?: any;
   value?: any;
+  focus: () => void;
 }
 /**
  * Extended React.SyntheticEvent whose target can have 'name' and 'value' fields.
  */
 interface InputEvent extends SyntheticEvent<HTMLDivElement> {
-  target: InputEventTarget;
+  target: CustomEventTarget;
+}
+
+interface ButtonClickEvent
+  extends React.MouseEvent<HTMLButtonElement, MouseEvent> {
+  target: CustomEventTarget;
 }
 
 /**
  * Custom Input event handler with name and value fields on the event target.
  */
 export type InputHandler = (e: InputEvent) => void;
+export type ButtonClickHandler = (e: ButtonClickEvent) => void;
+
 /**
  * Custom Submission handler which can be called programmatically without supplying an event.
  */
